4799fb63ecc41527805295c78158cc0c68edc5d6
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-05-19 21:59:21 +0300

    change code

--- The Age Calculator/app.js
+++ The Age Calculator/app.js
@@ -5,7 +5,7 @@ function CalculateYear(birthYear,futureYear){
 }
 if (futureYear>birthYear) {
   let year=CalculateYear(birthYear,futureYear);
-  console.log(`If you were born in ${birthYear}, then in ${futureYear} you'll be either ${year} or ${year+1}, depending on what month it is in ${futureYear}.`);   
+  console.log(`If you were born in ${birthYear}, then in ${futureYear} you'll be either ${year} or ${year-1}, depending on what month it is in ${futureYear}.`);   
 }else{
     console.log('Sorry, I couldn\'t calculated, because you were not born on that date ')
 }
