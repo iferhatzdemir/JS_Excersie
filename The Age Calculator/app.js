558b6e63f74ad234c1833a5d311dfa2e45d90b0c
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-05-19 20:41:27 +0300

    I am solved  problem in teaching materaials

---
+++ The Age Calculator/app.js
@@ -0,0 +1,11 @@
+let birthYear=1997;
+let futureYear=1996;
+function CalculateYear(birthYear,futureYear){
+    return futureYear-birthYear;
+}
+if (futureYear>birthYear) {
+  let year=CalculateYear(birthYear,futureYear);
+  console.log(`If you were born in ${birthYear}, then in ${futureYear} you'll be either ${year} or ${year+1}, depending on what month it is in ${futureYear}.`);   
+}else{
+    console.log('Sorry, I couldn\'t calculated, because you were not born on that date ')
+}
