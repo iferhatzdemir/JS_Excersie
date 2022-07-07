7961462ae107134408fe1d741ad2c9b8de5fa017
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-07-07 12:56:03 +0300

    I solved array execersis

---
+++ Control Flow & Arrays/Exercises: Arrays/app.js
@@ -0,0 +1,17 @@
+let choices = ["red", "orange", "pink", "yellow", "brown"];
+for (var i = 0; i < choices.length; i++) {
+  console.log(`My #${i + 1}  choice is  ${choices > [i]}`);
+}
+
+for (let i = 0; i < choices.length; i++) {
+  let choicenum = i + 1;
+  let choiceNumSuffix =
+    choicenum == 1
+      ? "st"
+      : choicenum == 2
+      ? "nd"
+      : choicenum == 3
+      ? "rd"
+      : "th";
+  console.log(`My ${choicenum}${choiceNumSuffix} choice is   ${choices[i]}`);
+}
